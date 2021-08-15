2e13be2466604bec429174d5e8a761a13674b85b
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-08-15 23:17:12 +0100

    💡 naming conventions

---
+++ variables.js
@@ -0,0 +1,21 @@
+// let x;
+// console.log(x);
+// x = 7;
+
+// variables can only be declared once and assigned multiple times
+let x = 7;
+x = 6;
+x = 5;
+x = 4;
+// let x = 8;
+let y = 3;
+let z = x + y;
+console.log('Answer: ' + z);
+
+// cannot use keywords for variable names 
+// let let = 8
+
+let firstNumber = 7;
+let secondNumber = 3;
+let firstName = 'Bob';
+let zipCode = '75082';
