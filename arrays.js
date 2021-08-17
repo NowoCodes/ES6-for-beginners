1426cea01574eba768c777fb453b164977d71066
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-08-17 13:49:40 +0100

    💡using arrays

---
+++ arrays.js
@@ -0,0 +1,36 @@
+let a = [4, 8, 15, 16, 23, 42];
+let b = ['david', 'eddie', 'alex', 'michael'];
+
+// console.log(a[0])
+// console.log(a[1])
+// console.log(a[2])
+// console.log(a)
+
+// reassigning an array value
+// a[0] = 7;
+// console.log(a);
+
+// object
+// console.log(typeof a);
+
+// let c = [4, 'alex', true];
+// console.log(c)
+
+// undefined
+// console.log(b[5])
+
+// console.log(a.length); // actual number, NOT zero based
+
+// a[10] = 77;
+// console.log(a)
+// console.log(a.length)
+
+a.push(77);
+console.log(a)
+console.log(a.length)
+
+a.pop();
+a.pop();
+a.pop();
+console.log(a)
+console.log(a.length)
