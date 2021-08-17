e63c7518b53b8e67217628b2d06dc5c6b9da02bd
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-08-17 01:33:10 +0100

    💡 types of operators

---
+++ operators.js
@@ -0,0 +1,32 @@
+// Categories of Operators
+
+// Assignment               =
+// Arithmetic               + - * /
+// var m = 10 % 3;
+// console.log(m)
+
+
+// Increment / Decrement    ++  --
+// var a = 1;
+// a++;
+// // console.log(a++)
+// console.log(++a)
+// console.log(a)
+
+// String                   '' +
+
+// Precedence
+// let b = (1 + 2) * 3;
+// console.log(b)
+
+// Function invocation operators    ()
+// console.log('')
+
+// Logical and: && or: ||
+
+// Member accessor operator         .
+// console.log()
+
+// Code block operators        { }
+
+// Array element access operators      []
