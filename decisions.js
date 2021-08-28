ecba97ab6f6850c01420b790d7e5cd9c411467c6
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-08-28 11:54:32 +0100

    💡 conditioinal statements

---
+++ decisions.js
@@ -0,0 +1,42 @@
+// if 
+
+// let count = 3;
+// if (count == 4) {
+//   console.log('Count is 4');
+// } else if (count > 4) {
+//   console.log('Count is GREATER than 4');
+// } else if (count < 4) {
+//   console.log('Count is LESS than 4');
+// } else {
+//   console.log('Count is NOT 4');
+// }
+
+
+
+// switch
+
+// let hero = 'Superman';
+// switch (hero.toLowerCase()) {
+//   case 'superman':
+//     console.log('super strength');
+//     console.log('x-ray vision strength');
+//     break;
+//   case 'batman':
+//     console.log('intelligence');
+//     console.log('fighting skills');
+//     break;
+//   default:
+//     console.log('member of JLA');
+//     break;
+// }
+
+
+// ternary operator 
+
+let a = 1, b = '1';
+// let result = (a == b) ? 'equal' : 'inequal';
+// let result = (a === b) ? 'equal' : 'inequal';
+// let result = (a != b) ? 'not equal' : 'equal';
+let result = (a !== b) ? 'not equal' : 'equal';
+console.log(result);
+// console.log(a == b ? 'equal' : 'inequal');
